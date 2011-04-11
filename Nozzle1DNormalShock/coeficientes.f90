86672ea8843163015ee9486e0de856be6e104e48
pekov <pekov@pekov-PC>
2011-04-11 00:03:42 -0300

    a

--- Nozzle1DNormalShock/coeficientes.f90
+++ Nozzle1DNormalShock/coeficientes.f90
@@ -44,35 +44,59 @@ contains
 
   subroutine coeficientes_e_fontes_qml
 	
+	real*8 teste1, teste2, teste3
+	real*8,dimension(:),allocatable :: Mx, Mex, ap, aex, aw, bp, uexx, afux, atux, btux, btrux  ! fluxo de massa na face leste
+	allocate (Mx(N),Mex(N), ap(N), aex(N), aw(N), bp(N), uexx(N), afux(N), atux(N), btux(N), btrux(N))
 	! Calculando o fluxo de massa na face leste
 	do i = 1, N
 	   Me(i) = ro*ue(i)*Ae(i) 
+	   Mex(i) = Me(i)
+	   Mx(i) = M(i)
 	end do
-
+   uexx = ue
 	! Fictício esquerdo P = 1
     awu(1) = 0.0d0
     aeu(1) = -1.0d0
     aPu(1) = 1.0d0
-    bPu(1) = 2.0d0*Uin
+    bPu(1) = (2.0d0*x(2)/(x(3)-x(2)))*(u(3)-u(2))
 	
 	! volumes internos
     do i = 2, N-1
-       afu(i)  = 0 
-	   atu(i)  = 0 
-	   btu(i)  = 0 
+      afu(i)  = (Pi/8.0d0)*ro*fator*u(i)*(2.0d0*raio(i))*deltax
+	   atu(i)  = M(i)/deltat
+	   btu(i)  = (M(i)*u_o(i))/deltat
 	   bpru(i) = - A(i)*(p(i+1)-p(i-1))/2.0d0
 
-	   awu(i)  = Me(i-1)
-	   aeu(i)  = 0 
-       aPu(i)  = Me(i)
-	   bpu(i)  = bpru(i)
+	   awu(i)  = Me(i-1) + mi*Ae(i-1)/deltax
+	   aeu(i)  = mi*Ae(i)/deltax
+	   
+      aPu(i)  = awu(i) + aeu(i) + afu(i) + atu(i)  
+	   bpu(i)  = btu(i) + bpru(i)
+	   aex(i) = aeu(i)
+	   ap(i) = aPu(i)
+	   aw(i) = awu(i)
+	   bp(i) = bpu(i)
 	end do
       
     ! Fictício direito P = N
-    awu(N) = 1.0d0
+    awu(N) = -1.0d0
     aeu(N) = 0.0d0
     aPu(N) = 1.0d0
-    bPu(N) = u(N-1)-u(N-2)
+    bPu(N) = (2.0d0*(x(N)-x(N-1))/(x(N-1)-x(N-2)))*(u(N-1)-u(N-2))
+    
+    aex(1) = aeu(1)
+    aw(1) = awu(1)
+    ap(1) = aPu(1)
+    bp(1) = bpu(1)
+    afux = afu
+    atux = atu
+    btux = btu
+    btrux = bpru
+    aex(N) = aeu(N)
+    aw(N) = awu(N)
+    ap(N) = aPu(N)
+    bp(N) = bpu(N)
+!write(*,*), 'cacareco'
 
   end subroutine coeficientes_e_fontes_qml
 
@@ -112,29 +136,38 @@ contains
   
   subroutine velocidades_ue
 
+      real*8 treta
     real*8  :: sigmap, sigmaE ! auxiliar
-
+   real*8,dimension(:),allocatable :: uex, ue0x      ! solução numéri
+   allocate (uex(N), ue0x(N))
+   
 	! Calculando ue no passo deltat+1
 	ue(1) = Uin
 	
 	do i = 2, N-2
 	   sigmap = awu(i)*u(i-1) + aeu(i)*u(i+1)
 	   sigmaE = awu(i+1)*u(i) + aeu(i+1)*u(i+2)
-	   ue(i) = (sigmap+sigmaE - 2.0d0*Ae(i)*(p(i+1)-p(i)))/(apu(i)+apu(i+1))
+	   ue(i) = (sigmap+sigmaE+((M(i)+M(i+1))/deltat)*ue_o(i) - 2.0d0*Ae(i)*(p(i+1)-p(i)))/(apu(i)+apu(i+1))
 	end do
 	
+	
 	ue(N-1) = (u(N-1)+u(N))/2.0d0
-	
+	treta = Ae(N-1)
 	Fat = (Uin*Ae(1))/(ue(N-1)*Ae(N-1))
-	
   	ue(N-1) = Fat*ue(N-1)
-
+	treta = ue(N-1)
+	ue0x = ue_o
+uex = ue
+!write(*,*), 'eta'
   end subroutine velocidades_ue
 
 !-------------------------------------------------
 
   subroutine coeficientes_simplec
 
+    real*8,dimension(:),allocatable :: dpx, dex      ! solução numéri
+   allocate (dpx(N),dex(N))
+   
     do i = 2, N-1
 	   ds(i) = A(i)/(apu(i)-awu(i)-aeu(i))
     end do
@@ -146,6 +179,9 @@ contains
 	! Calculando nos contornos
 	de(1) = ds(1+1)
 	de(N-1) = ds(N-1)
+	dpx = ds
+	dex = de
+!write(*,*), 'eta'
 
   end subroutine coeficientes_simplec
 
@@ -153,6 +189,12 @@ contains
 
   subroutine coeficientes_fontes_massa
 
+    real*8 teste1, teste2
+   !real*8,dimension(:),allocatable :: appl, awpl, awpl, bppl
+   real*8,dimension(:),allocatable :: aepl, bppl, awpl, appl, uexx, Aexx
+   !allocate (appl(N) , awpl(N)) , awpl(N) , bppl(N))
+   allocate (aepl(N) , bppl(N), awpl(N) , appl(N), uexx(N) , Aexx(N))
+   
     ! Calculando fictício P = 1	
     awplinha(1) = 0.0d0
     aeplinha(1) = 0.0d0
@@ -161,9 +203,11 @@ contains
 
     ! Calculando os volumes internos
     do i = 2, N-1
-       awplinha(i) = de(i-1)*Ae(i-1)
+      awplinha(i) = de(i-1)*Ae(i-1)
 	   aeplinha(i) = de(i)*Ae(i)
 	   aPplinha(i) = awplinha(i) + aeplinha(i)
+	   teste1 = ue(i-1)*Ae(i-1)
+	   teste2 = ue(i)*Ae(i)
 	   bpplinha(i) = ue(i-1)*Ae(i-1) - ue(i)*Ae(i)
     end do
   
@@ -172,28 +216,43 @@ contains
     aeplinha(N) = 0.0d0
     aPplinha(N) = 1.0d0
     bpplinha(N) = 0.0d0
+    uexx = ue
+    appl = aPplinha
+    awpl = awplinha
+    aepl = aeplinha
+    bppl = bpplinha
+    Aexx = Ae
+!write(*,*), 'eta'
 
   end subroutine coeficientes_fontes_massa
 
 !-------------------------------------------------
 
   subroutine atualizar_ficticios_massa
-
+real*8 teste1, teste2
 	! Calculando fictício P = 1
 	plinha(1) = 2.0d0*plinha(1+1) - plinha(1+2)
 
 	! Calculando fictício P = N 
 	plinha(N) = 2.0d0*plinha(N-1) - plinha(N-2)    
-
+	teste1 = plinha(1)
+	teste2 = plinha(N)
+!write(*,*), 'eta'
   end subroutine atualizar_ficticios_massa 
 
 !-------------------------------------------------
 
   subroutine corrigir_pressao
     
+  real*8,dimension(:),allocatable :: opa, ppl
+   allocate (opa(N), ppl(N))
+    
 	do i = 1, N
 	   p(i) = p(i) + plinha(i)
 	end do 
+	opa = p
+	ppl=plinha
+!write(*,*), 'eta'
 
   end subroutine corrigir_pressao
 
@@ -201,9 +260,14 @@ contains
   
   subroutine corrigir_velocidades
      
+   real*8,dimension(:),allocatable :: vel
+   real*8 teste1, teste2
+   allocate (vel(N))
 	! Calculando fictício P = 1
+	teste1 = u(2)
+	teste2 = 2.0d0*Uin
 	u(1) = - u(1+1) + 2.0d0*Uin
-
+   
 	! Calculando para volumes internos
 	do i = 2, N-1
 	   u(i) = u(i) - ds(i)*(plinha(i+1)-plinha(i-1))/2.0d0
@@ -211,6 +275,8 @@ contains
 
 	! Calculando fictício P = N
 	u(N) = u(N-1) + (u(N-1)-u(N-2))
+	vel = u
+!write(*,*), 'eta'
 
   end subroutine corrigir_velocidades
 
@@ -218,10 +284,20 @@ contains
 
   subroutine corrigir_velocidades_faces
 
+  real*8 testex, teste2, teste3
+   real*8,dimension(:),allocatable :: faces
+   allocate (faces(N) )
+faces = ue
 	! Calculando ue somente para volumes internos
     do i = 2, N-2
+    testex = ue(i)
+    teste2 = plinha(i+1)
+    teste3 = plinha(i)
 	   ue(i) = ue(i) - de(i)*(plinha(i+1)-plinha(i))
+	   
 	end do 
+	faces = ue
+!write(*,*), 'eta'
 
   end subroutine corrigir_velocidades_faces
 
